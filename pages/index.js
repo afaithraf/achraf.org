2d92a2582b579526e94455a5030454c2df7177f2
Ashraf <54310457+afaithraf@users.noreply.github.com>
2020-11-18 13:20:12 +0100

    mailchimp testing

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,24 @@
 import Head from 'next/head'
 import { NextSeo } from 'next-seo';
 import MailForm from '../components/MailForm';
-import { useEffect } from 'react';
-const Home = () => {
 
-  useEffect(() => {
-    !function (c, h, i, m, p) { m = c.createElement(h), p = c.getElementsByTagName(h)[0], m.async = 1, m.src = i, p.parentNode.insertBefore(m, p) }(document, "script", "https://chimpstatic.com/mcjs-connected/js/users/36615fd00beb84aa5c413348b/a33f19b55feb10e14dd8180d9.js");
-    return;
-  }, []);
-  return (
-    <div className="container">
-      <NextSeo
+const Home = () => (
+  <div className="container">
+    <NextSeo
         title="Official Website Of Achraf El Ghinoussi | Achraf.org"
         description="Achraf El Ghinoussi, better known as Achraf, is a Moroccan Developer, Entrepreneur. Achraf.org also provide a list of tools, freebies, and courses."
         keywords="Achraf, Achraf El Ghinoussi, developer, web, self-employed, yonayn, blog, courses, free"
         author="Yonyayn - Achraf El Ghinoussi - http://dev.achraf.org"
       />
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <MailForm />
+    <Head>
+      <link rel="icon" href="/favicon.ico" />
+      <script id="mcjs" dangerouslySetInnerHTML={{ __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/36615fd00beb84aa5c413348b/a33f19b55feb10e14dd8180d9.js");` }}>
+      </script>
+    </Head>
 
-      <style jsx global>{`
+    <MailForm />
+
+    <style jsx global>{`
       html,
       body {
         padding: 0;
@@ -34,7 +31,7 @@ const Home = () => {
         box-sizing: border-box;
       }
     `}</style>
-    </div>
-  )
-};
+  </div>
+)
+
 export default Home
