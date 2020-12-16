9bd5e142ac25b7ec6001895a039f9443bcca9391
Ashraf <54310457+afaithraf@users.noreply.github.com>
2020-12-16 21:18:54 +0100

    hello

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-  } from "react-router-dom";
-import Header from "./Header";
-import Home from "./Home";
-import Play from "./Play";
-import Footer from "./Footer";
-import '../css/App.scss';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import "../css/App.scss";
+import ReactPlayer from 'react-player'
 
 function App() {
-
-    const [myPick, setMyPick] = useState("");
-    const [housePick, setHousePick] = useState("");
-    const [gameScore, setGameScore] = useState(0);
-
-    function newHousePick() {
-        const choices = ["rock", "paper", "scissors"];
-        const randomChoice = choices[Math.floor((Math.random()*3))];
-        setHousePick(randomChoice);
-    }
-
-    useEffect(() => {
-        newHousePick();
-    },[setMyPick]);
-
-    return (
-         <Router>
-             <div className="wrapper">
-            <Header score={gameScore}/>
-            <Switch className="main">
-                <Route path="/play">
-                    <Play mine={myPick} house={housePick} score={gameScore} setScore={setGameScore} setHousePick={newHousePick}/>
-                </Route>
-                <Route path="/">
-                    <Home setPick={setMyPick} />
-                </Route>
-            </Switch>
-            <Footer />
-            </div>
-        </Router>
-    )
+  return (
+    <div className="wrapper">
+      <ReactPlayer
+        className="react-player"
+        url="https://www.youtube.com/watch?v=Z8H_mcvGXZA"
+        playing
+      />
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
